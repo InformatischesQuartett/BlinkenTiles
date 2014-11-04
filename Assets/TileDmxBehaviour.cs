17383f24be2bf32e49f5ec1124d8a3c353852878
Alexander Scheurer <mail@aspepex.net>
2014-11-04 10:17:14 +0100

    DMX Tiles

---
+++ Assets/TileDmxBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class TileDmxBehaviour : MonoBehaviour {
+
+    private Transform _reference;
+    private float _tileWidth;
+    private Material _materialOn;
+    private Material _materialIdle;
+    private Boolean _isOn;
+
+	// Use this for initialization
+	void Start () {
+        _reference = GameObject.Find("Reference").transform;
+        _tileWidth = transform.localScale.x * 10;
+        _materialIdle = new Material(Shader.Find("Sprites/Default"));
+        _materialIdle.SetColor("_Color", new Color(1, 1, 1));
+        _materialOn = new Material(Shader.Find("Sprites/Default"));
+        _materialOn.SetColor("_Color", new Color(0.3f, 0.3f, 1));
+        renderer.material = _materialIdle;
+	    _isOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((_reference.position.x > transform.position.x - _tileWidth / 2) && (_reference.position.x < transform.position.x + _tileWidth / 2) && !_isOn)
+        {
+            renderer.material = _materialOn;
+            _isOn = true;
+        }
+        else if (((_reference.position.x < transform.position.x - _tileWidth / 2) || (_reference.position.x > transform.position.x + _tileWidth / 2)) && _isOn)
+        {
+            renderer.material = _materialIdle;
+            _isOn = false;
+        }
+
+	}
+}
