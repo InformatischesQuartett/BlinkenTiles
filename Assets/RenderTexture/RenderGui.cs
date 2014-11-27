7c00bef0487b881c6237766ce40a436b7d5a0b63
Linda Schey <lindaschey@t-online.de>
2014-11-27 12:23:11 +0100

    Added first Lightning test

--- Assets/RenderTexture/RenderGui.cs
+++ Assets/RenderTexture/RenderGui.cs
@@ -21,31 +21,32 @@ public class RenderGui : MonoBehaviour {
 		//Width and Height of the RenderTextures are all the same
 		width = LeftRT.width;
 		height = LeftRT.height;
-
+		//float d = 5f/4f;
+		float maxWidth = (Screen.width/3);
 		//Set up the positions, wehre the textures are zuposed to be drawn in the GUI
-		RectL = new Rect(width/2,Screen.height/2-height/2,width, height);
-		RectC = new Rect(width+(width/2),Screen.height/2-height/2,width, height);
-		RectR = new Rect((2*width)+width/2,Screen.height/2-height/2,width, height);
+		RectL = new Rect(0,Screen.height/2-height/2,maxWidth, height);
+		RectC = new Rect(maxWidth,Screen.height/2-height/2,maxWidth, height);
+		RectR = new Rect(maxWidth*2,Screen.height/2-height/2,maxWidth, height);
 	}
 
 
 	void OnGUI()
 	{
 		//Rotate the Gui for the first RenderRexture (LeftRT)
-		GUIUtility.RotateAroundPivot(-90, RectL.center);
+		//GUIUtility.RotateAroundPivot(-90, RectL.center);
 		//Draw the RenderTexture
 		GUI.DrawTexture(RectL, LeftRT, ScaleMode.ScaleAndCrop);
 		//And because GUI in Unity is so awsome rotate the Gui back
-		GUIUtility.RotateAroundPivot(90, RectL.center);
+		//GUIUtility.RotateAroundPivot(90, RectL.center);
 
 		//And Again for the Center....
-		GUIUtility.RotateAroundPivot(-90, RectC.center);
+		//GUIUtility.RotateAroundPivot(-90, RectC.center);
 		GUI.DrawTexture(RectC, CenterRT, ScaleMode.ScaleAndCrop);
-		GUIUtility.RotateAroundPivot(90, RectC.center);
+		//GUIUtility.RotateAroundPivot(90, RectC.center);
 
 		//And Again for the Right....
-		GUIUtility.RotateAroundPivot(-90, RectR.center);
+		//GUIUtility.RotateAroundPivot(-90, RectR.center);
 		GUI.DrawTexture(RectR, RightRT, ScaleMode.ScaleAndCrop);
-		GUIUtility.RotateAroundPivot(90, RectR.center);
+		//GUIUtility.RotateAroundPivot(90, RectR.center);
 	}
 }
