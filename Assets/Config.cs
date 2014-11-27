894e7ae327bd10e66dc429db9907d7637c9f347b
Alexander Scheurer <mail@aspepex.net>
2014-11-27 13:20:41 +0100

    Tiles are instanciated and register themselves

--- Assets/Config.cs
+++ Assets/Config.cs
@@ -1,17 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public static class Config
 {
+    public static int Cols { get; private set; }
+    public static int Rows { get; private set; }
+    public static float TileWidth { get; private set; }
+    public static float TileHeight { get; private set; }
+    public static float TileSpaceing { get; private set; }
+
     public static Material MaterialWhite { get; private set; }
     public static Material MaterialYellow { get; private set; }
     public static Material MaterialRed { get; private set; }
     public static Material MaterialBlue { get; private set; }
     public static Material MaterialGreen { get; private set; }
     public static Material MaterialOrange { get; private set; }
+    public static ArrayList Sound { get; private set; }
 
     static Config()
     {
+        Cols = 8;
+        Rows = 4;
+        TileWidth = 270;
+        TileHeight = 90;
+        TileSpaceing = 10;
+
         MaterialWhite = new Material(Shader.Find("Sprites/Default"));
         MaterialWhite.SetColor("_Color", new Color(1, 1, 1));
         MaterialYellow = new Material(Shader.Find("Sprites/Default"));
@@ -25,5 +38,11 @@ public static class Config
         MaterialOrange = new Material(Shader.Find("Sprites/Default"));
         MaterialOrange.SetColor("_Color", new Color(1, 0.5f, 0,5f));
 
+        /*
+         * Sound[0] = GameObject.Find("Sound1");
+        Sound[1] = GameObject.Find("Sound2");
+        Sound[2] = GameObject.Find("Sound3");
+        Sound[3] = GameObject.Find("Sound4");
+        */
     }
 }
---
+++ Assets/TileController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileController : MonoBehaviour
+{
+
+    public GameObject TilePrefab;
+    private GameObject TileParent;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    TileParent = GameObject.Find("Tiles");
+
+	    BuildTiles();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BuildTiles()
+    {
+        float xInc = (Config.TileWidth + Config.TileSpaceing) / 10;
+        float yInc = (Config.TileHeight + Config.TileSpaceing) / 10;
+        float xStart = -((Config.Cols/2*xInc) - xInc/2);
+        float yStart = -((Config.Rows/2*yInc) - yInc/2);
+
+        Debug.Log(xStart + " : " + yStart + " | " + xInc + " : " + yInc);
+
+        for (int i = 0; i < Config.Cols; i++)
+        {
+            float yStartTmp = yStart;
+            for (int j = 0; j < Config.Rows; j++)
+            {
+                GameObject current = Instantiate(TilePrefab, new Vector3(xStart, yStartTmp, 0), Quaternion.Euler(270, 0, 0)) as GameObject;
+                
+                current.transform.localScale = new Vector3(Config.TileWidth / 100, 1, Config.TileHeight / 100);
+                current.transform.parent = TileParent.transform;
+
+                current.name = "Tile-" + i + "x" + j;
+
+                current.GetComponent<TileBehaviour>().SetTilePosition(i, j);
+
+                yStartTmp += yInc;
+            }
+            xStart += xInc;
+        }
+    }
+
+    public bool RegisterTile(int col, int row)
+    {
+        if (col >= 0 && col < Config.Cols && row >= 0 && row < Config.Rows)
+        {
+            Debug.Log("Tile registered.");
+            return true;
+        }
+        Debug.LogError("Tile NOT registered!");
+        return false;
+    }
+}
