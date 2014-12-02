19b24d4d8420992196389272f6254f43aa95e5c3
Alexander Scheurer <mail@aspepex.net>
2014-12-02 16:15:34 +0100

    It works like 4 weeks ago! :fu:

--- Assets/Helper.cs
+++ Assets/Helper.cs
@@ -1,4 +1,7 @@
-﻿public static class Helper {
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper {
 
     public static bool Between(this float num, float lower, float upper, bool inclusive = false)
     {
@@ -6,4 +9,27 @@
             ? lower <= num && num <= upper
             : lower < num && num < upper;
     }
+}
+struct TileCol
+{
+    public float XMin;
+    public float XMax;
+
+    public List<Tile> Tiles;
+}
+
+struct Tile
+{
+    public Rect Bounds;
+    public Rect TriggerBounds;
+
+    public GameObject TileGo;
+}
+
+public enum Highlighttype
+{
+    None,
+    Preview,
+    Time,
+    Hit
 }
