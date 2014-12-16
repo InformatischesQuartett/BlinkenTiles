50857db893f346008a67cc5fc0bf8f22dbf90737
Alexander Scheurer <mail@aspepex.net>
2014-12-16 16:09:55 +0100

    Prepared DmxController script, removed old stuff

---
+++ Assets/DmxController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DmxController : MonoBehaviour
+{
+    // Time Reference and FieldSize is set by the TileController
+    public float TimeReference { get; set; }
+    public float FieldSize { get; set; }
+
+    //Tick is called by the TileController
+    public void Tick()
+    {
+        //do DMX stuff here
+    }
+}
