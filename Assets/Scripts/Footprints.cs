f647cb69c0bf85180ffc2c919e3a01b9ff142987
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2015-01-17 16:02:25 +0100

    footprint graphics

---
+++ Assets/Scripts/Footprints.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Footprints : MonoBehaviour {
+
+	public Texture2D footprintImg;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI () {
+	}
+}
