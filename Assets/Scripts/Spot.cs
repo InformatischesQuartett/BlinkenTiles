88004759c1253039d39eebe5fc7010ef07f6c81a
Linda Schey <lindaschey@t-online.de>
2015-01-18 12:23:58 +0100

    cleand up

--- Assets/Scripts/Spot.cs
+++ Assets/Scripts/Spot.cs
@@ -1,46 +1,27 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+
 public class Spot
 {
-    private int position;
-    private List<byte> valueList;
-    private byte red, green, blue;
-    private byte intensity;
-    private byte[] _lightColor= new byte[4];
-    private CurrentCollor currentcolor;
+    private int _position;
+    private List<byte> _valueList;
+    private byte _red, _green, _blue;
+    private byte _intensity;
+    private CurrentCollor _currentcolor;
     public enum CurrentCollor
     {
         Red,Green,Blue,White, Black
     }
     public Spot(int p, ref List<byte> vList)
     {
-        position = p;
-        valueList = vList;
+        _position = p;
+        _valueList = vList;
 
-        red = blue = green = 0;
-        intensity = 20;
+        _red = _blue = _green = 0;
+        _intensity = 20;
 
         SetRowToColor(0,CurrentCollor.Black);
         SetRowToColor(1, CurrentCollor.Black);
         SetRowToColor(2, CurrentCollor.Black);
-        
-
-        /*valueList[position] = intensity;
-        valueList[position + 1] = 0;
-        valueList[position + 2] = 0;
-        valueList[position + 3] = 0;
-
-        valueList[position + 4] = intensity;
-        valueList[position+5] = 0;
-        valueList[position+6] = 0;
-        valueList[position+7] = 0;
-
-        valueList[position + 8] = intensity;
-        valueList[position + 9] = 0;
-        valueList[position + 10] = 0;
-        valueList[position + 11] = 0;*/
-       
     }
 
     public void LightItUp(int row)
@@ -50,51 +31,35 @@ public class Spot
             case 0:
                 for (int i = 0; i < 3; i++)
                 {
-                    valueList[position + i] = Config.LightColor[i];
+                    _valueList[_position + i] = Config.LightColor[i];
                 }
-                valueList[position + 3] = 0;
+                _valueList[_position + 3] = 0;
                 break;
             case 1:
                 for (int i = 4; i < 7; i++)
                 {
-                    valueList[position + i] = Config.LightColor[i - 4];
+                    _valueList[_position + i] = Config.LightColor[i - 4];
                 }
-                valueList[position + 7] = 0;
+                _valueList[_position + 7] = 0;
                 break;
             case 2:
                 for (int i = 8; i < 11; i++)
                 {
-                    valueList[position + i] = Config.LightColor[i - 8];
+                    _valueList[_position + i] = Config.LightColor[i - 8];
                 }
-                valueList[position + 7] = 0;
+                _valueList[_position + 7] = 0;
                 break;
             default:
-                valueList[position] = 1;
-                valueList[position + 1] = 1;
-                valueList[position + 2] = 1;
-                valueList[position + 3] = 0;
-
-                valueList[position + 4] = 1;
-                valueList[position + 5] = 1;
-                valueList[position + 6] = 1;
-                valueList[position + 7] = 0;
-
-                valueList[position + 8] = 1;
-                valueList[position + 9] = 1;
-                valueList[position + 10] = 1;
-                valueList[position + 11] = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    _valueList[_position + i] = 0;
+                }
                 break;
         }
     }
 
-    public void LightUp(int rowNr)
-    {
-        SetRowToColor(0, CurrentCollor.Black);
-        SetRowToColor(1, CurrentCollor.Black);
-        SetRowToColor(2, CurrentCollor.Black);
-        SetRowToColor(rowNr, CurrentCollor.Red);
-    }
-
+    
+    //can be used to set spot to one of the predefined Colors
     public void SetColor(CurrentCollor color)
     {
         switch (color)
@@ -125,6 +90,9 @@ public class Spot
                 SetRowToColor(2, CurrentCollor.Black);
                 break;
             default:
+                SetRowToColor(0, CurrentCollor.Black);
+                SetRowToColor(1, CurrentCollor.Black);
+                SetRowToColor(2, CurrentCollor.Black);
                 break;
         }   
     }   
@@ -134,62 +102,53 @@ public class Spot
         switch (color)
         {
             case CurrentCollor.Red:
-                red = intensity;
-                green = blue = 0;
+                _red = _intensity;
+                _green = _blue = 0;
                 break;
             case CurrentCollor.Green:
-                green = intensity;
-                red = blue = 0;
+                _green = _intensity;
+                _red = _blue = 0;
                 break;
             case CurrentCollor.Blue:
-                blue = intensity;
-                green = red = 0;
+                _blue = _intensity;
+                _green = _red = 0;
                 break;
             case CurrentCollor.White:
-                red = green = blue = intensity;
+                _red = _green = _blue = _intensity;
                 break;
             case CurrentCollor.Black:
-                red = green = blue = 0;
+                _red = _green = _blue = 0;
                 break;
             default:
+                _red = _green = _blue = 0;
                 break;
         }
 
         switch (row)
         {
             case 0:
-                valueList[position+0] = red;
-                valueList[position + 1] = green;
-                valueList[position + 2] = blue;
-                valueList[position + 3] = 0;
+                _valueList[_position+0] = _red;
+                _valueList[_position + 1] = _green;
+                _valueList[_position + 2] = _blue;
+                _valueList[_position + 3] = 0;
                 break;
             case 1:
-                valueList[position+4] = red;
-                valueList[position+5] = green;
-                valueList[position+6] = blue;
-                valueList[position + 7] = 0;
+                _valueList[_position+4] = _red;
+                _valueList[_position+5] = _green;
+                _valueList[_position+6] = _blue;
+                _valueList[_position + 7] = 0;
                 break;
             case 2:
-                valueList[position + 8] = red;
-                valueList[position + 9] = green;
-                valueList[position + 10] = blue;
-                valueList[position + 11] = 0;
+                _valueList[_position + 8] = _red;
+                _valueList[_position + 9] = _green;
+                _valueList[_position + 10] = _blue;
+                _valueList[_position + 11] = 0;
                 break;
             default:
-                valueList[position] = 1;
-                valueList[position + 1] = 1;
-                valueList[position + 2] = 1;
-                valueList[position + 3] = 0;
-
-                valueList[position+4] = 1;
-                valueList[position+5] = 1;
-                valueList[position+6] = 1;
-                valueList[position+7] = 0;
-
-                valueList[position + 8] = 1;
-                valueList[position + 9] = 1;
-                valueList[position + 10] = 1;
-                valueList[position + 11] = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    _valueList[_position + i] = 0;
+                }
                 break;
         }
     }
